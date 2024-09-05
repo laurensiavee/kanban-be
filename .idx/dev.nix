17359d2e99c589ff687226edfe1cca264554b7a3
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-09-05 14:26:27 +0000

    init mongodb

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,11 +4,15 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  packages = [ 
+    pkgs.python3
+    pkgs.mongodb
+    pkgs.mongosh
+   ];
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     # extensions = [ "ms-python.python" ];
-    extensions = [  ];
+    extensions = [  ]; 
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
@@ -17,7 +21,12 @@
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "README.md" "app/main.py" ];
       }; # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
+      onStart = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+          mongo-db = "chmod +x ./mongodb/start.sh && ./mongodb/start.sh"; 
+      };
+    }; 
     # Enable previews and customize configuration
     # previews = {
     #   enable = true;
---
+++ app/server/database.py
@@ -0,0 +1,6 @@
+import motor.motor_asyncio
+from bson.objectid import ObjectId
+
+MONGO_DETAILS = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
