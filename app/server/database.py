4b9efb877155c549f036b717a82f7588a437c6b0
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-09-17 16:15:23 +0000

    board CRUD

--- app/server/database.py
+++ app/server/database.py
@@ -12,25 +12,46 @@ board_collection = tasks_database.get_collection("board_collection")
 
 # helpers
 def board_helper(board) -> dict:
-    print("board")
-    print(board)
     return {
         "board_no": board["board_no"],
         "board_name": board["board_name"],
         "task_list": board["task_list"],
     }
 
-##### board CRUD
-# Retrieve all boards present in the database
-async def retrieve_boards():
+# Board
+async def get_list_board():
     boards = []
     async for board in board_collection.find():
         boards.append(board_helper(board))
     return boards
 
-# Add a new board into to the database
+async def get_board(board_no: str) -> dict:
+    board = await board_collection.find_one({"board_no": board_no})
+    if board:
+        return board_helper(board)
+
 async def add_board(board_data: dict) -> dict:
     board = await board_collection.insert_one(board_data)
     new_board = await board_collection.find_one({"_id": board.inserted_id})
     return board_helper(new_board)
+
+async def update_board(board_no: str, data: dict):
+    if len(data) < 1:
+        return False
+    board = await board_collection.find_one({"board_no": board_no})
+    if board:
+        updated_board = await board_collection.update_one(
+            {"board_no": board_no}, {"$set": data}
+        )
+        if updated_board:
+            return True
+        return False
+
+async def delete_board(board_no: str):
+    board = await board_collection.find_one({"board_no": board_no})
+    if board:
+        await board_collection.delete_one({"board_no": board_no})
+        return True
+
+
 
--- app/server/routes/board.py
+++ app/server/routes/board.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 from ..database import (
-    retrieve_boards,
+    get_list_board,
+    get_board,
     add_board,
+    update_board,
+    delete_board
 )
 
 from ..models.board import (
@@ -14,15 +17,49 @@ from ..models.board import (
 
 router = APIRouter()
 
-@router.get("/", response_description="Boards retrieved")
-async def get_boards():
-    boards = await retrieve_boards()
+@router.get("/list", response_description="Boards retrieved")
+async def get_list_board_data():
+    boards = await get_list_board()
     if boards:
         return ResponseModel(boards, "Boards data retrieved successfully")
     return ResponseModel(boards, "Empty list returned")
+
+@router.get("/{board_no}", response_description="Board data retrieved")
+async def get_board_data(board_no):
+    board = await get_board(board_no)
+    if board:
+        return ResponseModel(board, "Board data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Board doesn't exist.")
 
 @router.post("/", response_description="Board data added into the database")
 async def add_board_data(board: BoardSchema = Body(...)):
     board = jsonable_encoder(board)
     new_board = await add_board(board)
     return ResponseModel(new_board, "Board added successfully.")
+
+@router.put("/{board_no}")
+async def update_board_data(board_no: str, req: BoardSchema = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_board = await update_board(board_no, req)
+    if updated_board:
+        return ResponseModel(
+            "Board with ID: {} name update is successful".format(board_no),
+            "Board name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the board data.",
+    )
+
+@router.delete("/{board_no}", response_description="Board data deleted from the database")
+async def delete_board_data(board_no: str):
+    deleted_board = await delete_board(board_no)
+    if deleted_board:
+        return ResponseModel(
+            "Board with ID: {} removed".format(board_no), "Board deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "Board with id {0} doesn't exist".format(board_no)
+    )
+
